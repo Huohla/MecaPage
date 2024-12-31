1d3b714258cf1069d94c283533a12f711f7ac318
Lorena Rojas Padron <lorena.rojas.padron@nter.es>
2024-12-31 14:36:50 +0100

    Initialized Git repository

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import './style.css'
+import mecaLogo from './mecapage.svg';
+import { setupCounter } from './counter.js'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="${mecaLogo}" class="logo" alt="MecaPage logo" />
+    </a>
+    <h1>Hello Vite!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))
