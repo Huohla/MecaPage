ebea666ce5730d72f0a1f71dd56357639f48e358
Lorena Rojas Padron <lorena.rojas.padron@nter.es>
2025-01-01 17:01:46 +0100

    Start HTML and CSS

--- src/main.js
+++ src/main.js
@@ -1,20 +1,23 @@
-import './style.css'
-import mecaLogo from './mecapage.svg';
-import { setupCounter } from './counter.js'
+// import './style.css'
+// import mecaLogo from './mecapage.svg';
+// import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${mecaLogo}" class="logo" alt="MecaPage logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// document.querySelector('#app').innerHTML = `
+//   <div>
+//     <a href="https://vite.dev" target="_blank">
+//       <img src="${mecaLogo}" class="logo" alt="MecaPage logo" />
+//     </a>
+//     <h1>Hello Vite!</h1>
+//     <div class="card">
+//       <button id="counter" type="button"></button>
+//     </div>
+//     <div class="card">
+//       <button id="navigate" type="button"></button>
+//     </div>
+//     <p class="read-the-docs">
+//       Click on the Vite logo to learn more
+//     </p>
+//   </div>
+// `
 
-setupCounter(document.querySelector('#counter'))
+// setupCounter(document.querySelector('#counter'))
